bdb81af636f88bb531a0a79218d9de9fd0afe9fa
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-14 02:16:41 +0530

    Updated the register page and route.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const userRouter = require('./routes/user.routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
 app.set('view engine', 'ejs');
 
 app.use('/user', userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/test", (req,res) =>{
-    res.send("User Test Route");
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'Register' });
+});
+
+router.post('/register' , (req, res) => {
+  console.log('User registration data:', req.body);
+  res.send('User registered successfully');
 });
 
 module.exports = router;
