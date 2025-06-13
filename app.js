397597c946ae9f9b792d3793159b5c47ad6aef21
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-13 12:07:26 +0530

    Initial project setup.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRouter = require('./routes/user.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('/user', userRouter);
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
