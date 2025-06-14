9cb8f4dab28e31f0b6f1db8c500293b1c0d072f4
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-14 18:39:50 +0530

    Initial build of login page.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 router.get('/register', (req, res) => {
@@ -32,11 +33,63 @@ router.post('/register',
       username,
       email,
       password: hashPassword
-      
+
     })
 
     res.json(newUser);
 
   });
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+router.post('/login',
+  body('username').trim().isLength({ min: 3 }).withMessage('Invalid email address'),
+  body('password').trim().isLength({ min: 5 }).withMessage('Password must be at least 6 characters long'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Invalid input data'
+      });
+    }
+
+    const { username, password } = req.body;
+
+    const user = await userModel.findOne({
+      username: username
+    })
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+      return res.status(400).json({
+        message: 'Invalid username or password'
+      });
+    }
+
+    const token = jwt.sign({
+      userId: user._id,
+      email: user.email,
+      username: user.username
+    },
+      process.env.JWT_SECRET,
+  
+    )
+
+    res.json({
+      token
+    })
+
+  }
+)
 
 module.exports = router;
