2116994a98c5e879d2767ae2ef069c153d9f5e54
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-15 22:42:18 +0530

    Created Homepage Structure.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/user.routes');
 const dotenv = require('dotenv');
 const connectToDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index.routes');
 
 
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
+app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 
